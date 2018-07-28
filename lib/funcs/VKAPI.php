d58b45e1c9d082f0cc18ba225d1a31c36d4b5978
Sominemo <dilongzhenya@gmail.com>
2018-07-28 12:06:39 +0300

    VK Token movments

--- lib/funcs/VKAPI.php
+++ lib/funcs/VKAPI.php
@@ -1,9 +1,9 @@
 <?php
 
 class __VKAPI_DATA {
-    const DOMAIN = "https://api.vk.com/method/";
-    const VERSION = "5.80";
-    const TOKEN = EGV::VK_TOKEN;
+    private const DOMAIN = "https://api.vk.com/method/";
+    private const VERSION = "5.80";
+    private const TOKEN = EGV::VK_TOKEN;
 }
 
 class VKAPI extends __VKAPI_DATA {
@@ -14,17 +14,10 @@ class VKAPI extends __VKAPI_DATA {
         
     }
 
-    static function __call($name, $arguments)
+    static function call($name, $s)
     {
-        return __VKAPI_METHOD_HANDLER($name, $arguments);
-    }
-}
-
-class __VKAPI_METHOD_HANDLER extends __VKAPI_DATA {
-    static function __call($name, $arguments)
-    {
-        $l = self::DOMAIN.$arguments[0].".".$name;
-        $s = $arguments[1];
+        $l = self::DOMAIN.$name;
+        if (!is_array($s)) return false;
         if (!isset($s['access_token'])) $s['access_token'] = self::TOKEN;
         if (!isset($s['v'])) $s['v'] = self::VERSION;
         $t = Network::Request($l, ['AS_JSON' => true, 'POST' => $s]);
--- lib/user_set.php
+++ lib/user_set.php
@@ -7,7 +7,17 @@ class EGV {
       {
             if (!self::$init) {
             $r = json_decode(file_get_contents("priv/settings.json"));
-            if (json_last_error() !== JSON_ERROR_NONE) throw new Exception("Error Processing Settings", 2);      
+            if (json_last_error() !== JSON_ERROR_NONE) throw new Exception("Error Processing Settings", 2);  
+            foreach ($r as $k => $v) {
+                  if (is_array($v) && isset($v['action'])) {
+                        switch ($v['action']) {
+                              case 'readFile':
+                                    $r[$k] = file_get_contents($v['file']);
+                                    break;
+                        }
+                  }
+            }    
+            self::$data = $r;
             self::$init = true; 
             }
 
